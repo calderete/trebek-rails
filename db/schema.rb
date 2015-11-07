f2c628c8d569065c3d24eb752d77fc877a8ab40e
Carl Westmoreland <beguira86@gmail.com>
2015-11-07 17:34:07 -0500

    CONFLICTS

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151105192347) do
 
   create_table "decks", force: :cascade do |t|
     t.string   "title",      null: false
+    t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
