f69ee134e26a135eae7597d019cc3f4892f84418
Charles <codprestige10@gmail.com>
2015-11-08 18:10:34 -0500

    still working on votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,25 @@
 class VotesController < ApplicationController
 
-	def up_vote
+	def initialize
 		@user = current_user
-		@deck = Deck.find(params[:id])
-		@vote = @deck.liked_by @user
-		binding.pry
+		@deck = Deck.find(params[:deck_id])
+	end
+
+	def up_vote
+		@deck.liked_by @user
+		index
+	end
+
+	def down_vote
+		@deck.disliked_by @user
+		index
+	end
+
+	def index
+		@votes = Vote.all
+		render "votes.json.jbuilder"
 	end
 end
+		
+
+		
